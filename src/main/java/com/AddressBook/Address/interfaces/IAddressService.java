7d9b0a91bd20d2d8e9c77721399a0ce7ffec364a
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-08 14:13:24 +0530

    Added Get in Service

--- src/main/java/com/AddressBook/Address/interfaces/IAddressService.java
+++ src/main/java/com/AddressBook/Address/interfaces/IAddressService.java
@@ -1,12 +1,13 @@
 package com.AddressBook.Address.interfaces;
 
+import com.AddressBook.Address.dto.AddressDTO;
 import com.AddressBook.Address.model.Address;
 
 import java.util.List;
 
 public interface IAddressService {
-    Address save(Address address);
-    List<Address> getAll();
-    Address getById(Long id);
+    AddressDTO save(AddressDTO addressDTO);
+    List<AddressDTO> getAll();
+    AddressDTO getById(Long id);
     void delete(Long id);
 }
--- src/main/java/com/AddressBook/Address/service/AddressService.java
+++ src/main/java/com/AddressBook/Address/service/AddressService.java
@@ -1,25 +1,46 @@
 package com.AddressBook.Address.service;
 
+import com.AddressBook.Address.dto.AddressDTO;
 import com.AddressBook.Address.interfaces.IAddressService;
 import com.AddressBook.Address.model.Address;
 import com.AddressBook.Address.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressService implements IAddressService {
-    private final AddressRepository repository;
 
-    public AddressService(AddressRepository repository) {
-        this.repository = repository;
+    private final AddressRepository addressRepository;
+
+    @Autowired
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
     }
 
-    public List<Address> getAll() { return repository.findAll(); }
 
-    public Address getById(Long id) { return repository.findById(id).orElse(null); }
+    @Override
+    public List<AddressDTO> getAll() {
+        return addressRepository.findAll().stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
 
-    public Address save(Address address) { return repository.save(address); }
+    @Override
+    public AddressDTO getById(Long id) {
+        return addressRepository.findById(id)
+                .map(this::convertToDTO)
+                .orElse(null);
+    }
 
-    public void delete(Long id) { repository.deleteById(id); }
+
+    private AddressDTO convertToDTO(Address address) {
+        return new AddressDTO(address.getId(), address.getName(), address.getPhone(), address.getEmail(), address.getCity());
+    }
+
+    private Address convertToEntity(AddressDTO dto) {
+        return new Address(dto.getId(), dto.getName(), dto.getPhone(), dto.getEmail(), dto.getCity());
+    }
 }

be35fb9512d0188428fb07733385a96da26866a3
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-10 23:36:59 +0530

    Added Redis

--- src/main/java/com/AddressBook/Address/AddressApplication.java
+++ src/main/java/com/AddressBook/Address/AddressApplication.java
@@ -2,12 +2,14 @@ package com.AddressBook.Address;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
+// @EnableRedisHttpSession  // Uncomment if you want explicit control over session configuration
 public class AddressApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressApplication.class, args);
 	}
-
 }
--- src/main/java/com/AddressBook/Address/service/AddressService.java
+++ src/main/java/com/AddressBook/Address/service/AddressService.java
@@ -5,6 +5,9 @@ import com.AddressBook.Address.interfaces.IAddressService;
 import com.AddressBook.Address.model.Address;
 import com.AddressBook.Address.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,31 +15,40 @@ import java.util.stream.Collectors;
 
 @Service
 public class AddressService implements IAddressService {
-    @Autowired
-    AddressRepository addressRepository;
 
+    @Autowired
+    private AddressRepository addressRepository;
+
+    // When saving a new address or updating an existing one, update the cache.
     @Override
+    @CachePut(value = "addresses", key = "#result.id")
     public AddressDTO save(AddressDTO addressDTO) {
         Address address = convertToEntity(addressDTO);
         Address savedAddress = addressRepository.save(address);
         return convertToDTO(savedAddress);
     }
 
+    // Cache the result of the getAll method.
     @Override
+    @Cacheable(value = "addresses")
     public List<AddressDTO> getAll() {
         return addressRepository.findAll().stream()
                 .map(this::convertToDTO)
                 .collect(Collectors.toList());
     }
 
+    // Cache individual address retrieval using the address ID as key.
     @Override
+    @Cacheable(value = "addresses", key = "#id")
     public AddressDTO getById(Long id) {
         return addressRepository.findById(id)
                 .map(this::convertToDTO)
                 .orElse(null);
     }
 
+    // Clear the cached address entry when it is deleted.
     @Override
+    @CacheEvict(value = "addresses", key = "#id")
     public void delete(Long id) {
         addressRepository.deleteById(id);
     }
